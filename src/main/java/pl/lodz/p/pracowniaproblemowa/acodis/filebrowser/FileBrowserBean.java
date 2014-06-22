ccf55b68a99cb3f0339f80c04e141e0af1597a03
Paweł Tarasiuk <pablo.t89@gmail.com>
2014-06-22 12:06:00 +0200

    Łaaaał można usuwać i dodawać pliki i katalogi.

--- src/main/java/pl/lodz/p/pracowniaproblemowa/acodis/filebrowser/FileBrowserBean.java
+++ src/main/java/pl/lodz/p/pracowniaproblemowa/acodis/filebrowser/FileBrowserBean.java
@@ -1,6 +1,7 @@
 package pl.lodz.p.pracowniaproblemowa.acodis.filebrowser;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Map;
 
@@ -20,6 +21,36 @@ public class FileBrowserBean extends FileBrowserEntry implements Serializable {
     
     if(attributes.containsKey("resource")) {
       setResource((String) attributes.get("resource"));
+      
+      if(attributes.containsKey("action")) {
+        String action = (String) attributes.get("action");
+        
+        if(action.equals("rmdir") || action.equals("rm")) {
+          if(attributes.containsKey("referrer")) {
+            File f = new File(decodePath((String) attributes.get("referrer")));
+            f.delete();
+          }
+        }
+        
+        if(action.equals("new")) {
+          
+          if(attributes.containsKey("referrer") && attributes.containsKey("type") && attributes.containsKey("name")) {
+            String referrer = (String) attributes.get("referrer");
+            String type = (String) attributes.get("type");
+            String name = (String) attributes.get("name");
+            File p = new File(decodePath(referrer));
+            File n = new File(p, name);
+            if(type.equals("file")) {
+              try {
+                n.createNewFile();
+              } catch (Throwable e) { }
+            } else if(type.equals("dir")) {
+              n.mkdirs();
+            }
+            setResource(encodePath(n.getAbsolutePath()));
+          }
+        }
+      }
     }
   }
   
